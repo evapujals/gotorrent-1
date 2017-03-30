745185c824671087155ed263908073d9572cf13f
kibelon <kibelon@hotmail.es>
2017-03-30 11:44:05 +0200

    Script now opens a terminal for each actor

--- graphs/script.sh
+++ graphs/script.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
-python trackerG.py
+gnome-terminal -e "python trackerG.py"
 
-python peerG.py $1 torrentHash 10 seedFile
-python peerG.py $1 torrentHash 10 & > dades1
-python peerG.py $1 torrentHash 10 & > dades2
-python peerG.py $1 torrentHash 10 & > dades3
-python peerG.py $1 torrentHash 10 & > dades4
-python peerG.py $1 torrentHash 10 & > dades5
-python peerG.py $1 torrentHash 10 & > dades6
-python peerG.py $1 torrentHash 10 & > dades7
-python peerG.py $1 torrentHash 10 & > dades9
-python peerG.py $1 torrentHash 10 & > dades8
-python peerG.py $1 torrentHash 10 & > dades10
-python peerG.py $1 torrentHash 10 & > dades11
-python peerG.py $1 torrentHash 10 & > dades12
-python peerG.py $1 torrentHash 10 & > dades13
-python peerG.py $1 torrentHash 10 & > dades14
-python peerG.py $1 torrentHash 10 & > dades15
+gnome-terminal -e "python peerG.py $1 torren1 10 seedFile"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
+gnome-terminal -e "python peerG.py $1 torren1 10"
